4e4dddde25a81435fda12c8359ecb175d71d7e0e
ahmad75reza <ahmad75reza>
2023-03-18 12:19:05 +0530

    check Authentication without sign in

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,28 @@
+const Post = require('../models/post');
+
+
+
 module.exports.home = function (req, res) {
     // return res.end('<h1> Express is up for codeial!</h1>');
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function (err, posts) {
+
+    //     // return res.render('home', {
+    //     //     title: "(parameter) posts: any[]",
+    //     //     posts: posts
+    //     // });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            title: "(parameter) posts: any[]",
+            posts: posts
+        });
     });
+
 }
 
 //module.exports.actionName = function(req, res){}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,60 @@
-const Post =require('../models/post');
+// const Post = require('../models/post');
+// // const User = require('../models/user');
 
-module.exports.create = function(req, res){
+// module.exports.create = function (req, res) {
+//     console.log(req.body);
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user
+//     }, function (err, post) {
+//         if (err) {
+//             console.log('error in creating a post in controller');
+//             return res.status(500).json({
+//                 error: "Internal Server Error"
+//             });
+//         }
+
+//         Post.populate(post, { path: 'user', select: '_id name email' }, function(err, post) {
+//             if (err) {
+//                 console.log('error in populating post user info');
+//                 return res.status(500).json({
+//                     error: "Internal Server Error"
+//                 });
+//             }
+
+//             return res.status(200).json({
+//                 message: "Post created successfully"
+//             });
+//         });
+//     });
+// }
+
+
+
+
+
+
+
+const Post = require('../models/post');
+//const user = require('../models/user');
+
+module.exports.create = function (req, res) {
+    console.log(req.body);
     Post.create({
+
         content: req.body.content,
         user: req.user._id
-    }, function(err, post){
-        if(err){
+    }, function (err, post) {
+        if (err) {
             console.log('error in creating a post in controller');
             return;
         }
         return res.redirect('back');
     });
-    
+
 }
+
+
+
+
 

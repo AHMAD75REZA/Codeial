edbb645a17cd0d93e076e602021076dd2a150e1b
ahmad75reza <ahmad75reza>
2023-04-05 02:16:18 +0530

    Social Authentication using google passpot

--- index.js
+++ index.js
@@ -9,20 +9,22 @@ const session = require('express-session')
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
-const path =require('path');
+const path = require('path');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
 
 
 app.use(sassMiddleware({
-    src:"./assets/scss",
+    src: "./assets/scss",
     dest: "./assets/css",
-    debug:true,
-    outputStyle:'extended',
-    prefix:'/css'
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
 }));
 
 app.use(express.urlencoded());
@@ -31,7 +33,7 @@ app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
-app.use('/uploads', express.static(__dirname+ '/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(expresslayouts);
 //extract style and scripts from sub pages into the layout

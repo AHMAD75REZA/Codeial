6c29f244c3471949300ae0ffaec0e1747ff06773
ahmad75reza <ahmad75reza>
2023-04-06 01:52:25 +0530

    sending Email through google sign in/up

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -42,7 +42,7 @@
 
 const Comment = require('../models/comments');
 const Post = require('../models/post');
-
+const commentsMailer = require('../mailers/comments_mailer');
 module.exports.create = async function (req, res) {
 
     try {
@@ -57,6 +57,20 @@ module.exports.create = async function (req, res) {
 
             post.comments.push(comment);
             post.save();
+
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
 
             req.flash('success', 'Comment published!');
 
